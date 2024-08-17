37413db820c45b9cf2fff979d969cdc11530ebe1
DannyFeng <FengzihangCode@outlook.com>
2024-08-17 14:26:22 +0800

    Initial Commit

---
+++ Photo Organizer/Photo_OrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_OrganizerApp.swift
+//  Photo Organizer
+//
+//  Created by feng on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Photo_OrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
